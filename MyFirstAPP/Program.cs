ee91e34b8c625ea59e104ead12e52955720ac4e6
RobinLRV <robin.ellervee@gmail.com>
2023-09-10 19:23:23 +0300

    create new app

---
+++ MyFirstAPP/Program.cs
@@ -0,0 +1,17 @@
+﻿// Start
+
+// Nime kysimine ja tervitamine
+
+Console.WriteLine("Please enter you name");
+
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello, " + userName + "!");
+
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !");
+
+
+
+
